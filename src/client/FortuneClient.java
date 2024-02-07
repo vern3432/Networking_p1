de4d63df8879c146e4892ce6ce14e51c70465921
Aidan_Varano <varanoab@merrimack.edu>
2024-02-07 14:49:05 -0500

    Madeconditions for sending messages back and forth

--- src/client/FortuneClient.java
+++ src/client/FortuneClient.java
@@ -96,7 +96,32 @@ public class FortuneClient extends JFrame {
             socket = new Socket("127.0.0.1", 5000);
             receiver = new Scanner(socket.getInputStream());
             sender = new PrintWriter(socket.getOutputStream(), true);
+
+            new Thread(this::recieveMessages).start();
         } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Failed to connect to the server.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void recieveMessages(){
+        try{
+            while(receiver.hasNextLine()){
+                String response = receiver.nextLine();
+                SwingUtilities.invokeLater(() -> responseArea.append(response + "\n"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void dispose(){
+        super.dispose();
+        try{
+            if(socket != null) socket.close();
+            if(sender != null) sender.close();
+            if(receiver != null) receiver.close();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
