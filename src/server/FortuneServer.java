62f3d4a28c9473421b72270c384b8e9a69b452d5
Aidan_Varano <varanoab@merrimack.edu>
2024-02-07 18:52:21 -0500

    Added proccessing of datbase and atuhors

--- src/server/FortuneServer.java
+++ src/server/FortuneServer.java
@@ -8,11 +8,24 @@ import java.io.PrintWriter;
 import java.lang.Double;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashMap; // import the HashMap class
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import merrimackutil.json.JsonIO;
 import merrimackutil.json.types.*;
 
 public class FortuneServer {
+
+  public static ArrayList<String> getAllAuthors(
+    HashMap<String, List<String>> quotesByAuthor
+  ) {
+    ArrayList<String> authorNames = new ArrayList<>(quotesByAuthor.keySet());
+    return authorNames;
+  }
 
   public static void main(String[] args) {
     try {
@@ -27,22 +40,57 @@ public class FortuneServer {
         }
       }
       JSONObject configJsonObject = JsonIO.readObject(configFile);
-      String dbFilePath = "src/server/"+configJsonObject.get("fortune-database").toString();
+      String dbFilePath =
+        "src/server/" + configJsonObject.get("fortune-database").toString();
       @SuppressWarnings("deprecation")
       Double port = new Double(configJsonObject.get("port").toString());
       @SuppressWarnings("deprecation")
-      Double pool_size = new Double(configJsonObject.get("pool-size").toString());
+      Double pool_size = new Double(
+        configJsonObject.get("pool-size").toString()
+      );
       int portint = port.intValue();
       int pool_sizeint = pool_size.intValue();
-
       System.out.println(dbFilePath);
       File dbFile = new File(dbFilePath);
       JSONObject db = JsonIO.readObject(dbFile);
+      Object[] arraydb = db.getArray("quotes").toArray();
+      HashMap<String, List<String>> quotesByAuthor = new HashMap<>();
 
-      System.out.println(db.getArray("quotes").getObject(1));
+      for (Object obj : arraydb) {
+        JSONObject quoteObject = (JSONObject) obj;
+        String author = quoteObject.getString("author");
+        String quote = quoteObject.getString("quote");
+
+        // Check if the quote already exists for the author
+        if (quotesByAuthor.containsKey(author)) {
+          List<String> quotesList = quotesByAuthor.get(author);
+          // Check if the quote already exists in the list
+          if (!quotesList.contains(quote)) {
+            quotesList.add(quote);
+          }
+        } else {
+          // If the author does not exist in the map, create a new list and add the quote to it
+          List<String> quotesList = new ArrayList<>();
+          quotesList.add(quote);
+          quotesByAuthor.put(author, quotesList);
+        }
+      }
+      //printing db by author
+      for (Map.Entry<String, List<String>> entry : quotesByAuthor.entrySet()) {
+        String author = entry.getKey();
+        List<String> quotesList = entry.getValue();
+
+        System.out.println("Author: " + author);
+        System.out.println("Quotes:");
+        for (String quote : quotesList) {
+          System.out.println("- " + quote);
+        }
+      }
+      ArrayList<String> authorNames = getAllAuthors(quotesByAuthor);
 
 
-
+      //map/values gives you all values as list, just do this as random
+      //quotes
 
       //start server
       ServerSocket server = new ServerSocket(portint);
