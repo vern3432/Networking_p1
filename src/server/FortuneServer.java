a5d1c99adcbcb5cd419e195535cfb831eae922e5
Aidan_Varano <varanoab@merrimack.edu>
2024-02-07 18:01:44 -0500

    JSONS read correclty now

--- src/server/FortuneServer.java
+++ src/server/FortuneServer.java
@@ -1,23 +1,53 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.Double;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import merrimackutil.json.JsonIO;
 import merrimackutil.json.types.*;
-import merrimackutil.json.types.JSONArray;
+
 public class FortuneServer {
 
   public static void main(String[] args) {
     try {
-      ServerSocket server = new ServerSocket(5000);
+      String configFilePath = "src/server/config.json";
+      File configFile = new File(configFilePath);
+      try (
+        BufferedReader br = new BufferedReader(new FileReader(configFilePath))
+      ) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          System.out.println(line);
+        }
+      }
+      JSONObject configJsonObject = JsonIO.readObject(configFile);
+      String dbFilePath = "src/server/"+configJsonObject.get("fortune-database").toString();
+      @SuppressWarnings("deprecation")
+      Double port = new Double(configJsonObject.get("port").toString());
+      @SuppressWarnings("deprecation")
+      Double pool_size = new Double(configJsonObject.get("pool-size").toString());
+      int portint = port.intValue();
+      int pool_sizeint = pool_size.intValue();
 
-      File configFile = new File("./config.json");
-      JSONArray test= new JSONArray();
-      
-      // Loop forever handling connections.
+      System.out.println(dbFilePath);
+      File dbFile = new File(dbFilePath);
+      JSONObject db = JsonIO.readObject(dbFile);
+
+      System.out.println(db.getArray("quotes").getObject(1));
+
+
+
+
+      //start server
+      ServerSocket server = new ServerSocket(portint);
+      System.out.println("Server Running at port:" + portint);
+      // Loop forever handling connections.(Integer)
       while (true) {
         Socket sock = server.accept();
 
